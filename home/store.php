7eee2d79c1e54018bb554574e415c5cc02d626f5
Shane Tampipig <shaneamety@gmail.com>
2025-07-18 23:53:32 +0800

    store responsiveness

--- home/store.php
+++ home/store.php
@@ -26,6 +26,7 @@ $categories_result = $conn->query($categories_query);
     <meta charset="UTF-8">
     <title>OneUnit Left - Store</title>
     <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
