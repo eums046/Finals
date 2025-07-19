6ca25132844c3eec5f55e3d76c16edd3b7ecbc6e
Shane Tampipig <shaneamety@gmail.com>
2025-07-19 17:44:06 +0800

    shshs

--- home/verify.php
+++ home/verify.php
@@ -11,6 +11,7 @@ if ($conn->connect_error) {
 <head>
     <meta charset="UTF-8">
     <title>Email Verification</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,9 +1,11 @@
 <?php
+$host = "sql107.infinityfree.com";
 $user = "if0_39501475";                
 $pass = "2FaKH0u92yc";   
 $dbname = "if0_39501475_oneunit_left";  
 
-$conn = mysqli_connect($host, $user, $pass, $dbname);
+$conn = mysqli_connect("sql107.infinityfree.com", "if0_39501475", "2FaKH0u92yc", "if0_39501475_oneunit_left");
+
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
